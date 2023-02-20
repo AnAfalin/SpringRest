3a6d860ceaf28fc180694efd1f9ddb4963028053
Anastasia <an.afalin@yandex.ru>
2023-02-20 13:23:15 +0300

    initial commit

---
+++ src/test/java/ru/lazarenko/springrest/SpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lazarenko.springrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
