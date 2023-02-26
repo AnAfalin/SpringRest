d41237a507e9ceae7b5d0088cc790f58484db8b6
Anastasia <an.afalin@yandex.ru>
2023-02-26 22:58:14 +0300

    corrected part first

--- src/main/java/ru/lazarenko/springrest/controller/UserController.java
+++ src/main/java/ru/lazarenko/springrest/controller/UserController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(value = "/first/users", produces = "application/xml")
+@RequestMapping(value = "/first/users")
 public class UserController {
     private final UserService userService;
 
--- src/main/java/ru/lazarenko/springrest/entity/User.java
+++ src/main/java/ru/lazarenko/springrest/entity/User.java
@@ -1,7 +1,5 @@
 package ru.lazarenko.springrest.entity;
 
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -12,11 +10,9 @@ import lombok.*;
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
-@JacksonXmlRootElement(localName = "User")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @JacksonXmlProperty
     private Integer id;
     private String name;
     private String login;
