8975e25dcee6f009703a31c27c9a990f8a2af487
Anastasia <an.afalin@yandex.ru>
2023-03-01 20:10:00 +0300

    fix errors and corrected them in the part second

--- src/main/java/ru/lazarenko/springrest/service/EmployeeService.java
+++ src/main/java/ru/lazarenko/springrest/service/EmployeeService.java
@@ -9,7 +9,6 @@ import ru.lazarenko.springrest.repository.EmployeeRepository;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,81 +17,48 @@ public class EmployeeService {
     private final DepartmentService departmentService;
 
     @Transactional
-    public void saveEmployee(Employee employee){
+    public void saveEmployee(Employee employee) {
         Department department = employee.getDepartment();
-        if(department != null){
+        if (department != null && department.getId() != null) {
             department = departmentService.getDepartmentById(department.getId());
-            department.addEmployee(employee);
-            departmentService.updateDepartment(department.getId(), department);
             employee.setDepartment(department);
         }
-//        employeeRepository.save(employee);
+        employeeRepository.save(employee);
     }
 
     @Transactional(readOnly = true)
-    public List<Employee> getAllEmployees(){
+    public List<Employee> getAllEmployees() {
         return employeeRepository.findAllEmployees();
     }
 
     @Transactional(readOnly = true)
-    public Employee getEmployeeById(Integer id){
+    public Employee getEmployeeById(Integer id) {
         return employeeRepository.findEmployeeById(id)
                 .orElseThrow(() -> new NoSuchElementException(
                         "Employee with id='%d' not found".formatted(id)));
     }
 
     @Transactional
-    public void updateEmployee(Integer id, Employee employee){
+    public void updateEmployee(Integer id, Employee employee) {
         Employee updatableEmployee = employeeRepository.findById(id)
                 .orElseThrow(() -> new NoSuchElementException(
                         "Employee with id='%d' not found".formatted(id)));
 
-        Department department = departmentService.getDepartmentById(employee.getDepartment().getId());
-        if(department != null){
-            department = departmentService.getDepartmentById(department.getId());
-        }
-
         updatableEmployee.setName(employee.getName());
         updatableEmployee.setEmail(employee.getEmail());
-        updatableEmployee.setDepartment(department);
 
+        Department department = employee.getDepartment();
+
+        if (department != null && department.getId() != null) {
+            department = departmentService.getDepartmentById(department.getId());
+
+        }
+        updatableEmployee.setDepartment(department);
         employeeRepository.save(updatableEmployee);
     }
 
     @Transactional
-    public void deleteEmployee(Integer id){
+    public void deleteEmployee(Integer id) {
         employeeRepository.deleteById(id);
-    }
-
-    @Transactional(readOnly = true)
-    public List<Employee> getEmployeesByDepartmentId(Integer id) {
-        return employeeRepository.findAllEmployeesByDepartmentId(id);
-    }
-
-
-    @Transactional
-    public void addEmployeeByDepartmentId(Employee employee, Integer departmentId) {
-        Department department = departmentService.getDepartmentById(departmentId);
-        if(department != null){
-            department = departmentService.getDepartmentById(department.getId());
-            employee.setDepartment(department);
-        }
-        employeeRepository.save(employee);
-    }
-
-    @Transactional
-    public void deleteEmployeeByDepartmentId(Integer employeeId, Integer departmentId) {
-        Employee employee = employeeRepository.findById(employeeId)
-                .orElseThrow(() -> new NoSuchElementException(
-                        "Employee with id='%d' not found".formatted(employeeId)));
-
-        Department department = departmentService.getDepartmentById(departmentId);
-        if(department != null){
-            department = departmentService.getDepartmentById(department.getId());
-            department.deleteEmployee(employee);
-            departmentService.updateDepartment(departmentId, department);
-        }
-        employee.setDepartment(null);
-        employeeRepository.save(employee);
     }
 }
